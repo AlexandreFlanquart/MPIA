f7bf4016c5d6bffbe66cf5fb0bdc4f9e39bb9235
AlexandreFlanquart <104308588+AlexandreFlanquart@users.noreply.github.com>
2023-01-19 22:49:43 +0100

    :tada: Initial commit

---
+++ Source/MPIA/MPIA.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MPIA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MPIA, "MPIA" );
+
+DEFINE_LOG_CATEGORY(LogMPIA)
+ 
